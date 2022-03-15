cef4b62a01284937e2d0a6eb6a928875d33d8794
mihai.berende <mihai.berende@zitec.com>
2022-03-15 15:19:52 +0200

    remove main

--- pages/other-page.tsx
+++ pages/other-page.tsx
@@ -1,19 +1,17 @@
 import type { NextPage } from 'next';
-
 import { MainLayout } from '@/components/layouts/main';
-
-import { Heading, Box, Text } from '@chakra-ui/react';
+import { Heading, VStack, Text } from '@chakra-ui/react';
 
 const Other: NextPage = () => {
   return (
     <MainLayout>
-      <Box as="main" width="100%" height="100%">
+      <VStack width="100%" height="100%">
         <Heading> Some title here </Heading>
         <Text>
           Some dummy content here dummy content here dummy content here dummy
           content here
         </Text>
-      </Box>
+      </VStack>
     </MainLayout>
   );
 };
